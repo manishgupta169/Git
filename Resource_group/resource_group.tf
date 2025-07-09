51ff21c7feb3a3dd7205343a18c55fc4a3a5152a
Manish gupta <manishgupta169@hotmail.com>
2025-07-09 22:46:42 +0530

    rg9

--- Resource_group/resource_group.tf
+++ Resource_group/resource_group.tf
@@ -7,3 +7,9 @@ resource "azurerm_resource_group" "resource_test_7" {
   name     = var.resource_name
   location = var.resouce_location
 }
+
+
+resource "azurerm_resource_group" "resource_test_8" {
+  name     = var.resource_name
+  location = var.resouce_location
+}
